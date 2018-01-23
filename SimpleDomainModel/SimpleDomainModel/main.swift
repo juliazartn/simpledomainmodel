f6486c9f0fd9170fdfd84c57667204ad1518a43b
juliazartn <julia.zartn@gmail.com>
2018-01-23 14:10:18 -0800

    finished Money class and manually tested

--- SimpleDomainModel/SimpleDomainModel/main.swift
+++ SimpleDomainModel/SimpleDomainModel/main.swift
@@ -27,19 +27,58 @@ public struct Money {
   public var amount : Int
   public var currency : String
     
-  
-//  public func convert(_ to: String) -> Money {
-//
-//  }
-//
-//  public func add(_ to: Money) -> Money {
-//
-//  }
-//
-//  public func subtract(_ from: Money) -> Money {
-//
-//  }
+ //function converts the currency to USD
+    private func convertUS(_ to: String) -> Double {
+    switch currency {
+    case "USD":
+        return Double(self.amount)
+    case "GBP":
+        return Double(self.amount * 2)
+    case "EUR":
+        return Double(self.amount) / 1.5
+    case "CAN":
+        return Double(self.amount / 5 * 4)
+    default:
+        return 0
+    }
+  }
+    
+ //function converts US to new currency
+private func convertElse(from: Double, to: String) -> Double {
+    switch to {
+    case "USD":
+        return from
+    case "GBP":
+        return from / 2
+    case "EUR":
+        return from * 1.5
+    case "CAN":
+        return from * 1.25
+    default:
+        return 0
+    }
+  }
+    
+  public func convert(_ to: String) -> Money {
+    let toUS = convertUS(to)
+    let toNew = convertElse(from: toUS, to: to)
+    
+    return Money(amount: Int(toNew), currency: to)
+  }
+
+  public func add(_ to: Money) -> Money {
+    let old = convert(to.currency)
+    let newAmount = old.amount + to.amount
+    return Money(amount: newAmount, currency: to.currency)
+  }
+
+  public func subtract(_ from: Money) -> Money {
+    let old = convert(from.currency)
+    let newAmount = from.amount - old.amount
+    return Money(amount: newAmount, currency: from.currency)
+  }
 }
+
 
 ////////////////////////////////////
 // Job
